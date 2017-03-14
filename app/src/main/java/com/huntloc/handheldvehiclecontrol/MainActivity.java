15309b67490531028cb2060f046721f08ccfce49
dmoran <dmoran@huntloc.pe>
2017-03-14 16:59:46 -0500

    test

--- app/src/main/java/com/huntloc/handheldvehiclecontrol/MainActivity.java
+++ app/src/main/java/com/huntloc/handheldvehiclecontrol/MainActivity.java
@@ -1,12 +1,18 @@
 package com.huntloc.handheldvehiclecontrol;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.AsyncTask;
+import android.os.Parcelable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private static long back_pressed;
     private EditText editText_Plate;
     private Button button_Ckeck;
-
+    private NfcAdapter mNfcAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -87,7 +93,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "This device doesn't support NFC.",
+                    Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        if (!mNfcAdapter.isEnabled()) {
+            Toast.makeText(this, "Please enable NFC.", Toast.LENGTH_LONG)
+                    .show();
+        }
+        handleIntent(getIntent());
 
     }
 
@@ -115,8 +132,104 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setupForegroundDispatch(this, mNfcAdapter);
+    }
 
+    @Override
+    protected void onPause() {
+        stopForegroundDispatch(this, mNfcAdapter);
+        super.onPause();
+    }
 
+    public void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(),
+                activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(
+                activity.getApplicationContext(), 0, intent, 0);
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+        filters[0] = new IntentFilter();
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+
+         /*filters[0].addAction(NfcAdapter.ACTION_TECH_DISCOVERED);
+         String[][] techList = new String[][]{new String[]{NfcA.class.getName()}, new String[]{MifareClassic.class.getName()}, new String[]{NdefFormatable.class.getName()}};*/
+
+         /*filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+         try {
+             filters[0].addDataType(MIME_TEXT_PLAIN);
+         } catch (IntentFilter.MalformedMimeTypeException e) {
+             throw new RuntimeException("Check your mime type.");
+         }*/
+
+        filters[0].addAction(NfcAdapter.ACTION_TAG_DISCOVERED);
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters,
+                techList);
+    }
+    public void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+
+            Parcelable parcelable = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Tag tag = (Tag) parcelable;
+            byte[] id = tag.getId();
+            String code = getDec(id) + "";
+            Log.d("Internal Code", code);
+            editText_Plate.setText(code);
+        }
+
+           /* if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+                NdefMessage ndefMessage = null;
+                Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+                if ((rawMessages != null) && (rawMessages.length > 0)) {
+                    ndefMessage = (NdefMessage) rawMessages[0];
+                    String result = "";
+                    byte[] payload = ndefMessage.getRecords()[0].getPayload();
+                    String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+                    int languageCodeLength = payload[0] & 0077;
+                    //String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+                    String text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+                    Log.d("Internal Code", text);
+                    HandheldFragment handheldFragment = ((HandheldFragment) mSectionsPagerAdapter.getItem(0));
+                    if (handheldFragment != null) {
+                        handheldFragment.setCredentialId(text);
+                    }
+                }
+            }*/
+
+        /*if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+            Parcelable parcelable = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Tag tag = (Tag) parcelable;
+            byte[] id = tag.getId();
+            String code = getDec(id) + "";
+            Log.d("Internal Code", code);
+            HandheldFragment handheldFragment = ((HandheldFragment) mSectionsPagerAdapter.getItem(0));
+            if (handheldFragment != null) {
+                handheldFragment.setCredentialId(code);
+            }
+        }*/
+    }
+
+    private long getDec(byte[] bytes) {
+        long result = 0;
+        long factor = 1;
+        for (int i = 0; i < bytes.length; ++i) {
+            long value = bytes[i] & 0xffl;
+            result += value * factor;
+            factor *= 256l;
+        }
+        return result;
+    }
     @Override
     public void onBackPressed() {
         if (back_pressed + 2000 > System.currentTimeMillis())
